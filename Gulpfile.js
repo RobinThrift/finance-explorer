09d674c668467829c3066c4ce8643407cc3ee26b
Robin Thrift <robin@we-two-design.com>
2014-10-04 15:07:02 +0200

    added build tasks (traceur and linting)

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    config = {
+        dest: './dist',
+        paths: {
+            scripts: ['src/*.js', 'src/**/*.js']
+        }
+    };
+
+
+
+
+gulp.task('lint', function() {
+    var jshint = require('gulp-jshint');
+    return gulp.src(config.paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('scripts', ['lint'], function() {
+    var traceur = require('gulp-traceur');
+    return gulp.src(config.paths.scripts, {base: './src'})
+        .pipe(traceur())
+        .pipe(gulp.dest(config.dest));
+});
