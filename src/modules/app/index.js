386ba15b60ca5d00b50ac89ba52266329d362cd4
Robin Thrift <thrift.d.robin@googlemail.com>
2014-10-05 11:33:01 +0200

    added module registering to app

--- src/modules/app/index.js
+++ src/modules/app/index.js
@@ -1,12 +1,51 @@
 // src/modules/app/index.js
+var _ = require('lodash'),
+    util = require('util'),
+    forEach = require('each-async'),
+    EventEmitter = require('events').EventEmitter,
+    App;
+    
 
+App = function(opts) {
+    this.defaults = {
+        db: {
+            name: 'finances',
+            path: opts.cwd + '/finances.db'
+        }
+    };
+    this.opts = _.defaults(opts, this.defaults);
+    this.screen = this.opts.screen;
+    this.blessed = this.opts.blessed;
+    this.prg = this.opts.program;
+    this.cwd = opts.cwd;
 
-var App = function(opts) {
-    this.opts = opts;
+    this.modulesArr = [];
+    this.expose = {};
 };
 
-App.prototype.start = function() {
+util.inherits(App, EventEmitter);
 
+App.prototype.start = function() {
+    this.emit('before:start');
+
+    this.prg.alternateBuffer();
+    this.prg.disableMouse();
+    this.prg.hideCursor();
+    this.prg.clear();
+
+    this.setup();
+
+    this.emit('after:start');
+};
+
+App.prototype.setup = function() {
+    forEach(this.modulesArr, (module, index, done) => {
+        module.register(this, this.opts, this.screen, this.blessed, this.expose, done);
+    }); 
+};
+
+App.prototype.registerModule = function(module) {
+    this.modulesArr.push(module);
 };
 
 module.exports = App;
